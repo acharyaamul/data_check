2a80a81f4a36157363b4436ac6e7c3a9991e5f24
acharyaamul <acharya.amulkumar@gmail.com>
2023-08-03 20:16:10 +0545

    second commit

--- data_check2august2023.R
+++ data_check2august2023.R
@@ -2,7 +2,7 @@ setwd("~/00_GISDATA/00_Leaf/crystal/analysis_august/result/")
 list.files()
 nfi<-read.csv( "ceo-NFI-ART-TREES-Data-Collection-2012-2021-sample-data-2023-08-02.csv",header=T)
 names(nfi)
-# 9-7 point Intact forest
+# 9-7 points Intact forest
 # 6-4 points Degraded Forest
 # 1-3 points Very Degraded Forest
 nfi_leaf<-nfi[,c(1,2,3,4,12,20,21,36,46,48)]
@@ -10,6 +10,8 @@ nfi_leaf$status<-ifelse(nfi_leaf$Number.of.tree.covered.samples..2021.>6,"Intact
                         ifelse(nfi_leaf$Number.of.tree.covered.samples..2021.>3,"Degraded","Very Degraded"))
 library(dplyr)
 dat<-nfi_leaf %>% distinct(plotid, .keep_all = TRUE)
+
+
 
 dat1<-dat %>% group_by(status)%>% 
   summarise(biomasa_ad= mean(pl_total_biom_ad_ton_ha),
