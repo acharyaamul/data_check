b065fb56786294ce1f01f45b392bb053c30f47da
acharyaamul <acharya.amulkumar@gmail.com>
2023-08-03 20:25:43 +0545

    third commit

--- data_check2august2023.R
+++ data_check2august2023.R
@@ -1,5 +1,5 @@
 #### This is the code for checking data taken from CEO####
-nfi<-read.csv( "ceo-NFI-ART-TREES-Data-Collection-2012-2021-sample-data-2023-08-02.csv",header=T)
+nfi<-read.csv( "ceo-NFI-ART-TREES-Data-Collection-2012-2021-sample-data-2023-08-03.csv",header=T)
 names(nfi)
 # 9-7 points Intact forest
 # 6-4 points Degraded Forest
@@ -9,12 +9,8 @@ nfi_leaf$status<-ifelse(nfi_leaf$Number.of.tree.covered.samples..2021.>6,"Intact
                         ifelse(nfi_leaf$Number.of.tree.covered.samples..2021.>3,"Degraded","Very Degraded"))
 library(dplyr)
 dat<-nfi_leaf %>% distinct(plotid, .keep_all = TRUE)
-
-
-
 dat1<-dat %>% group_by(status)%>% 
   summarise(biomasa_ad= mean(pl_total_biom_ad_ton_ha),
             count=n())
-
 table(dat$State.of.forest.in.year.of.date_measurement)
 
